d44920e2ad89ba61242a9089ef15346020725c77
Rumi <76618296+RumiDalas@users.noreply.github.com>
2021-03-15 13:39:09 +0600

    Binary Search With Recursion Using Java

---
+++ BinarySearch.java
@@ -0,0 +1,57 @@
+
+public class BinarySearch {
+    
+   public void binarySearch(int arr[], int first, int last, int key){  
+   int mid = (first + last)/2;  
+   while( first <= last ){  
+      if ( arr[mid] < key ){  
+        first = mid + 1;     
+      }else if ( arr[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid - 1;  
+      }  
+      mid = (first + last)/2;  
+   }  
+   if ( first > last ){  
+      System.out.println("Element is not found!");  
+   }  
+    
+   }
+   
+   
+   //Recursive binary search
+   
+   public int  binarySearchRecursive(int arr[], int first, int last, int key){  
+        if (last>=first){  
+            int mid = first + (last - first)/2;  
+            if (arr[mid] == key){  
+            return mid;  
+            }  
+            if (arr[mid] > key){  
+            return binarySearchRecursive(arr, first, mid-1, key);//search in left subarray  
+            }else{  
+            return binarySearchRecursive(arr, mid+1, last, key);//search in right subarray  
+            }  
+        }  
+        return -1;  
+    }  
+    
+    
+    
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};  
+        int key = 30;  
+        int last=arr.length-1; 
+        BinarySearch obj = new BinarySearch();
+        obj.binarySearch(arr, 0, last, key);
+        //Recursive Binary 
+        int result =obj. binarySearchRecursive(arr,0,last,key);  
+        if (result == -1) { 
+            System.out.println("Element is not found!");  
+        } else  {
+            System.out.println("Element is found at index: "+result);  
+        }
+    }
+}
